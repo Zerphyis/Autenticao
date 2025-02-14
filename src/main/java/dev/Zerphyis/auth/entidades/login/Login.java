18d0ed393a2aef68e3dc15544b7929692087b5a7
Otavio <estudosprogotavio@gmail.com>
2025-02-14 16:38:34 -0300

    Entidade Login e sua Service

---
+++ src/main/java/dev/Zerphyis/auth/entidades/login/Login.java
@@ -0,0 +1,61 @@
+package dev.Zerphyis.auth.entidades.login;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Table(name = "tb_logados")
+@Entity
+public class Login implements UserDetails  {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String email;
+    private String senha;
+
+    public Login(String email, String senha) {
+        this.email = email;
+        this.senha = senha;
+    }
+    public  Login(){
+
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/dev/Zerphyis/auth/service/ServiceLogin.java
@@ -0,0 +1,37 @@
+package dev.Zerphyis.auth.service;
+
+import dev.Zerphyis.auth.entidades.login.Login;
+import dev.Zerphyis.auth.repositorios.RepositoryLogin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServiceLogin implements UserDetailsService {
+    @Autowired
+    RepositoryLogin repository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Login user = repository.findByEmail(username);
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getSenha(),
+                user.getAuthorities()
+        );
+    }
+
+
+    public boolean verifyPassword(String rawPassword, String encodedPassword) {
+        return passwordEncoder.matches(rawPassword, encodedPassword);
+    }
+}
+
