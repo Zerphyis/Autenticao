127ca00940faa272e4c96c01a9b37a5afc4e5f35
Otavio <estudosprogotavio@gmail.com>
2025-02-17 16:19:55 -0300

    Atualizando Entidade Login

--- src/main/java/dev/Zerphyis/auth/entidades/login/Login.java
+++ src/main/java/dev/Zerphyis/auth/entidades/login/Login.java
@@ -24,6 +24,22 @@ public class Login implements UserDetails  {
 
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of();
